4ef78554a4d5964684455d8d4b4eb592d8d60035
Muhammad Umer Farooq <mufkamran.dikhan.pk@gmail.com>
2022-09-25 11:38:47 +0500

    first page done

---
+++ script.js
@@ -0,0 +1,17 @@
+let menu=document.getElementsByClassName("product")[0]
+menu.addEventListener('click', function(){
+    let dropDown=document.getElementsByClassName("product-items")[0]
+    dropDown.classList.toggle("display-none")
+})
+
+let mediaMenuProduct=document.getElementsByClassName("product")[1]
+mediaMenuProduct.addEventListener('click', function(){
+    let dropDown=document.getElementsByClassName("product-items")[1]
+    dropDown.classList.toggle("display-none")
+})
+
+let mediaHeaderMenu=document.getElementsByClassName("fa-bars")[0]
+mediaHeaderMenu.addEventListener('click', function(){
+    let dropDown=document.getElementsByClassName("media-header-dropdown")[0]
+    dropDown.classList.toggle("display-none")
+})
