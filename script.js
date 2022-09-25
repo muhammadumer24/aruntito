2b61d9ded40b2891a31220e8239529f41bc54aea
Muhammad Umer Farooq <mufkamran.dikhan.pk@gmail.com>
2022-09-25 14:10:35 +0500

    hmm

--- script.js
+++ script.js
@@ -14,4 +14,7 @@ let mediaHeaderMenu=document.getElementsByClassName("fa-bars")[0]
 mediaHeaderMenu.addEventListener('click', function(){
     let dropDown=document.getElementsByClassName("media-header-dropdown")[0]
     dropDown.classList.toggle("display-none")
+    setTimeout(function(){
+        document.getElementsByClassName("product-items")[1].classList.toggle("actually-none")  
+    }, 300);
 })
